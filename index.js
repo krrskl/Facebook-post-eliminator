2c1732f55646bbbe275aed258ac5b9c1e7ae4f62
krrskl <carrascalruben@gmail.com>
2019-08-31 01:57:08 -0500

    :books: Adding documentation

---
+++ index.js
@@ -0,0 +1,51 @@
+var position = 0;
+var cont = 0;
+var numPosts = 0;
+
+function scrapePage() {
+  try {
+    document.getElementsByClassName("_2e42 _2yi0 _2yia")[position].click();
+    position += 9;
+    console.info(
+      `%cMarcando contenido #${numPosts + 1}`,
+      "border-radius: 5px;background: #4CAF50;padding: .5rem;width: 80%;color: white;font-weight: bold;font-size: medium;"
+    );
+    numPosts++;
+    setTimeout(() => {
+      scrapePage();
+    }, 700);
+  } catch (error) {
+    document.getElementsByClassName("_43rl")[4].click();
+
+    try {
+      /* document
+        .getElementsByClassName(
+          "layerCancel _4jy0 _4jy3 _4jy1 _51sy selected _42ft"
+        )
+        [cont].click(); */
+      cont++;
+    } catch (e) {
+      console.warn(" XXXXXX " + e + " XXXXXX");
+    }
+
+    position = 0;
+
+    setTimeout(() => {
+      //   scrapePage();
+    }, 10000);
+    console.log(
+      "%cEliminando contenido...",
+      "border-radius: 5px;background: #ff3628;padding: .5rem;width: 80%;color: white;font-weight: bold;font-size: medium;"
+    );
+  }
+}
+
+function start() {
+  console.info(
+    `%cIniciando...`,
+    "border-radius: 5px;background: #4CAF50;padding: .5rem;width: 80%;color: white;font-weight: bold;font-size: medium;"
+  );
+
+  numPosts = cont = position = 0;
+  scrapePage();
+}
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Eliminar publicaciones de forma automática en **Facebook Creator Studio**
+
+### Captura
+![Pantalla de bienvenida](https://i.imgur.com/aE1sCdd.png)
+
+### Uso
+
+1. Navegar a [Facebook Creator Studio](https://business.facebook.com/creatorstudio).
+
+2. Seleccionar la página a manejar y llevar el zoom del navegador hasta el **25%** combinación de teclas `Ctrl y -`
+
+3. Pulsar la combinación de teclas  `Ctrl + Shift + i` para abrir una consola de desarrollo.
+
+4. Pegar el código del archivo **index.js** en la consola de desarrollo abierta y luego pulsar la tecla `Enter`.
+
+5. Para iniciar el programa deberá escribir ahora `start()` en la consola y pulsar la tecla `Enter` inmediatamente el programa comenzará a marcar las publicaciones y a eliminar.
+
+
+### Información o reportes de error
+
+Este código fue creado y muy poco probado, cualquier problema que llegue a tener lo puede indicar en la [zona de problemas](https://github.com/krrskl/Facebook-post-eliminator/issues/new), si logra solucionarlo y quiere contribuir puede enviar su [PR](https://github.com/krrskl/Facebook-post-eliminator/pulls).
+
+
+> Hecho con ❤️ por [Rubén Carrascal](https://krrskl.github.io/)
